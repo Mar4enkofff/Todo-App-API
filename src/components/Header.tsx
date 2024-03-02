1841318a349900a5ffbb5fc1e7d7c451e73a4c3f
Oleksandr Marchenko <sasha@mate.academy>
2024-03-02 13:27:21 +0200

    Todo-App-API cloned

---
+++ src/components/Header.tsx
@@ -0,0 +1,98 @@
+import classNames from 'classnames';
+import React, { FormEvent, useState, useCallback, ChangeEvent } from 'react';
+import { Todo } from '../types/Todo';
+import { addTodo } from '../api/todos';
+import { ErrorTypes, USER_ID } from '../constants';
+
+type Props = {
+  title: string;
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  setErrorType: React.Dispatch<React.SetStateAction<string>>;
+  setTempTodo: React.Dispatch<React.SetStateAction<Todo | null>>;
+  toggleAll: () => void;
+  isToggleAllActive: boolean;
+};
+
+export const Header: React.FC<Props> = ({
+  title,
+  setTitle,
+  todos,
+  setTodos,
+  setTempTodo,
+  setErrorType,
+  toggleAll,
+  isToggleAllActive,
+}) => {
+  const [isTodoAdding, setIsTodoAdding] = useState(false);
+
+  const addTodoToServer = useCallback(async () => {
+    const newTodoToFetch = {
+      title,
+      completed: false,
+      userId: USER_ID,
+    };
+
+    const currentTempTodo = {
+      ...newTodoToFetch,
+      id: 0,
+    };
+
+    setTempTodo(currentTempTodo);
+    setIsTodoAdding(true);
+
+    try {
+      const addedTodo = await addTodo(newTodoToFetch);
+
+      setTodos(prevTodos => [...prevTodos, addedTodo]);
+    } catch (error) {
+      setErrorType(ErrorTypes.ADD);
+    } finally {
+      setIsTodoAdding(false);
+      setTempTodo(null);
+    }
+  }, [setErrorType, setTempTodo, setTodos, title]);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!title.trim()) {
+      setErrorType('empty title');
+
+      return;
+    }
+
+    setTitle('');
+    addTodoToServer();
+  };
+
+  const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  return (
+    <header className="todoapp__header">
+      {!!todos.length && (
+        <button
+          type="button"
+          aria-label="toggle all todos statuses"
+          className={classNames('todoapp__toggle-all', {
+            active: isToggleAllActive,
+          })}
+          onClick={toggleAll}
+        />
+      )}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={title}
+          onChange={handleTitleChange}
+          disabled={isTodoAdding}
+        />
+      </form>
+    </header>
+  );
+};
---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const USER_ID = 11335;
+
+export enum FilterValues {
+  ALL = 'all',
+  ACTIVE = 'active',
+  COMPLETED = 'completed',
+}
+
+export enum ErrorTypes {
+  ADD = 'add',
+  DELETE = 'delete',
+  UPLOAD = 'upload',
+  UPDATE = 'update',
+  NONE = '',
+}
